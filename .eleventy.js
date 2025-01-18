93ce3fd38197ec1ca7fb0d4fdec7407eae1d842d
SqrtNegativOne <sqrtnegativ1@gmail.com>
2025-01-18 23:45:48 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -121,7 +121,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output: "public",
+            output: "_site",
             includes: "_includes",
             data: "_data",
         },
